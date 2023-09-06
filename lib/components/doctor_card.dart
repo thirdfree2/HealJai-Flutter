1b35566c8e2f9eb1a0be39758c5544b34f62944a
thirdfree2 <pativeth.k@gmail.com>
2023-09-06 23:14:05 +0700

    lol

--- lib/components/doctor_card.dart
+++ lib/components/doctor_card.dart
@@ -1,86 +1,135 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../utils/config.dart';
+import 'package:http/http.dart' as http;
 
-class DoctorCard extends StatelessWidget {
-  const DoctorCard({Key? key, required this.route}) : super(key: key);
+class DoctorCard extends StatefulWidget {
+  const DoctorCard({Key? key}) : super(key: key);
 
-  final String route;
+  @override
+  State<DoctorCard> createState() => _DoctorCardState();
+}
+
+class _DoctorCardState extends State<DoctorCard> {
+  List<dynamic> psychonistList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTicket();
+  }
+
+  Future<void> fetchTicket() async {
+    final api1Url = 'http://10.1.203.91:3000/getpsychonist';
+
+    final api1Response = await http.get(Uri.parse(api1Url));
+
+    if (api1Response.statusCode == 200) {
+      final api1Json = jsonDecode(api1Response.body);
+      final api1Cars = api1Json['data'];
+
+      setState(() {
+        psychonistList = api1Cars;
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     Config().init(context);
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      height: 150,
-      child: GestureDetector(
-        child: Card(
-          elevation: 5,
-          color: Colors.white,
-          child: Row(
-            children: [
-              SizedBox(
-                width: 80,
-                child: Image.asset(
-                  'assets/images/doctor1.jpg',
-                  fit: BoxFit.fill,
-                ),
-              ),
-              Flexible(
-                  child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'Dr Ridchard Tan',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+    return Expanded(
+      // Wrap with Expanded
+      child: ListView.builder(
+        itemCount: psychonistList.length,
+        itemBuilder: (context, index) {
+          if (psychonistList.isEmpty) {
+            return CircularProgressIndicator();
+          }
+          final psychonist = psychonistList[index];
+          final name = psychonist['name'];
+          return Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            height: 150,
+            child: GestureDetector(
+              child: Card(
+                elevation: 5,
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 80,
+                      child: Image.asset(
+                        'assets/images/doctor1.jpg',
+                        fit: BoxFit.fill,
                       ),
                     ),
-                    Text(
-                      'Dental',
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.normal,
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width:
+                              100, // Provide width and height for the Container
+                          height: 100,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                'Dr Ridchard Tan',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                'Dental',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                              const Spacer(),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: const <Widget>[
+                                  Icon(
+                                    Icons.star_border,
+                                    color: Colors.yellow,
+                                    size: 16,
+                                  ),
+                                  Spacer(
+                                    flex: 1,
+                                  ),
+                                  Text('4.5'),
+                                  Spacer(
+                                    flex: 1,
+                                  ),
+                                  Text('Reviews'),
+                                  Spacer(
+                                    flex: 1,
+                                  ),
+                                  Text('(20)'),
+                                  Spacer(
+                                    flex: 7,
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
-                    const Spacer(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: const <Widget>[
-                        Icon(
-                          Icons.star_border,
-                          color: Colors.yellow,
-                          size: 16,
-                        ),
-                        Spacer(
-                          flex: 1,
-                        ),
-                        Text('4.5'),
-                        Spacer(
-                          flex: 1,
-                        ),
-                        Text('Reviews'),
-                        Spacer(
-                          flex: 1,
-                        ),
-                        Text('(20)'),
-                        Spacer(
-                          flex: 7,
-                        ),
-                      ],
                     )
                   ],
                 ),
-              ))
-            ],
-          ),
-        ),
-        onTap: () {
-          Navigator.of(context).pushNamed(route);
+              ),
+              onTap: () {
+                print('hello');
+              },
+            ),
+          );
         },
       ),
     );
--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/components/appoinment_card.dart';
 import 'package:flutter_application_1/components/doctor_card.dart';
 import 'package:flutter_application_1/utils/config.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  List<dynamic> psychologistList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPsychologists();
+  }
+
+  Future<void> fetchPsychologists() async {
+    final apiUrl = 'http://10.1.203.91:3000/getpsychonist';
+
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      final psychologistData = jsonData['data'];
+
+      setState(() {
+        psychologistList = psychologistData;
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
   final List<Map<String, dynamic>> medCat = [
     {
       "icon": FontAwesomeIcons.userDoctor,
@@ -23,137 +50,123 @@ class _HomePageState extends State<HomePage> {
     },
     {
       "icon": FontAwesomeIcons.lungs,
-      "category": "Respirations",
+      "category": "Respiratory",
     },
     {
-      "icon": FontAwesomeIcons.personPregnant,
-      "category": "Dermatolog",
+      "icon": FontAwesomeIcons.person,
+      "category": "Dermatologist",
     },
     {
-      "icon": FontAwesomeIcons.hand,
-      "category": "Dermatolog",
+      "icon": FontAwesomeIcons.handHoldingMedical,
+      "category": "Orthopedics",
     },
     {
-      "icon": FontAwesomeIcons.teeth,
+      "icon": FontAwesomeIcons.tooth,
       "category": "Dental",
     },
   ];
+
   @override
   Widget build(BuildContext context) {
     Config().init(context);
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 218, 245, 232),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 15,
-          vertical: 15,
-        ),
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      'Jessi',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(
-                      child: CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Config.primaryColor,
-                        backgroundImage: AssetImage('assets/images/helm2.jpg'),
-                      ),
-                    ),
-                  ],
-                ),
-                Config.spaceMedium,
-                const Text(
-                  'Category',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                Config.spaceSmall,
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.05,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: medCat.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        color: Config.primaryColor,
-                        margin: const EdgeInsets.only(right: 20),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 15, vertical: 5),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              FaIcon(
-                                medCat[index]['icon'],
-                                color: Colors.white,
-                              ),
-                              SizedBox(width: 5),
-                              Text(
-                                medCat[index]['category'],
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 16, left: 5, right: 5),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Search...",
-                      hintStyle: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: const Color.fromARGB(255, 255, 255, 255)),
-                      prefixIcon: Icon(
-                        Icons.search,
-                        color: const Color.fromARGB(255, 255, 255, 255),
-                        size: 20,
-                      ),
-                      filled: true,
-                      fillColor: Color.fromARGB(255, 65, 112, 98),
-                      contentPadding: EdgeInsets.all(8),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide(color: Colors.grey.shade100)),
-                    ),
-                  ),
-                ),
-                Config.spaceSmall,
-                const Text(
-                  'The Doctors',
+                Text(
+                  'Jessi',
                   style: TextStyle(
-                    fontSize: 16,
+                    fontSize: 24,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                Config.spaceSmall,
-                Column(
-                  children: List.generate(10, (index) {
-                    return DoctorCard(
-                      route: 'doc_details',
-                    );
-                  }),
-                )
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Config.primaryColor,
+                  backgroundImage: AssetImage('assets/images/helm2.jpg'),
+                ),
               ],
             ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Categories',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 100, // Set a specific height for the category list
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: medCat.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: Config.primaryColor,
+                  margin: const EdgeInsets.only(right: 20),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        FaIcon(
+                          medCat[index]['icon'],
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          medCat[index]['category'],
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'The Doctors',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: psychologistList.length,
+              itemBuilder: (BuildContext context, int index) {
+                final psychologist = psychologistList[index];
+                final name = psychologist['doc_username'];
+
+                return ListTile(
+                  leading: const Icon(Icons.list),
+                  trailing: const Text(
+                    "GFG",
+                    style: TextStyle(color: Colors.green, fontSize: 15),
+                  ),
+                  title: Text("$name"),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
